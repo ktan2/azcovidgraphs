d4f8021fa497db0ef044ebec80c18b14e3e85545
ktan2 <kara.tanoue@gmail.com>
2020-06-11 22:20:08 -0700

    publishing data...

---
+++ build_site.r
@@ -0,0 +1,9 @@
+# Build R Site Script
+# Author: Kara Haberstock Tanoue
+# Many thanks to Nick Strayer
+# 06.11.2020
+
+setwd("C:\\Users\\karat\\Box\\GIS Remote Work\\AZPIMA_COVID19\\azcovidgraphs")
+
+#render
+rmarkdown::render_site()
